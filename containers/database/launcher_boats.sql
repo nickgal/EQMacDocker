85130eb566be4e0237ed8d3f2adfe6164d817f81
Nick Gal <nickgal@me.com>
2023-08-02 00:36:02 +0000

    Update server, use new database dump

--- containers/database/launcher_boats.sql
+++ containers/database/launcher_boats.sql
@@ -1,12 +1,16 @@
-INSERT INTO `launcher_zones` VALUES ('boats', 'erudnext', '7375', '1', 'classic');
-INSERT INTO `launcher_zones` VALUES ('boats', 'erudsxing', '7376', '1', 'classic');
-INSERT INTO `launcher_zones` VALUES ('boats', 'qeynos', '7377', '1', 'classic');
-INSERT INTO `launcher_zones` VALUES ('boats', 'freporte', '7378', '1', 'classic');
-INSERT INTO `launcher_zones` VALUES ('boats', 'oot', '7379', '1', 'classic');
-INSERT INTO `launcher_zones` VALUES ('boats', 'butcher', '7380', '1', 'classic');
-INSERT INTO `launcher_zones` VALUES ('boats', 'oasis', '7381', '1', 'classic');
-INSERT INTO `launcher_zones` VALUES ('boats', 'nro', '7382', '1', 'classic');
-INSERT INTO `launcher_zones` VALUES ('boats', 'firiona', '7383', '1', 'kunark');
-INSERT INTO `launcher_zones` VALUES ('boats', 'overthere', '7384', '1', 'kunark');
-INSERT INTO `launcher_zones` VALUES ('boats', 'timorous', '7385', '1', 'kunark');
-INSERT INTO `launcher_zones` VALUES ('boats', 'iceclad', '7386', '1', 'velious');
+UPDATE `launcher_zones`
+SET `port` = CASE
+  WHEN `zone` = 'erudnext' THEN 7375
+  WHEN `zone` = 'erudsxing' THEN 7376
+  WHEN `zone` = 'qeynos' THEN 7377
+  WHEN `zone` = 'freporte' THEN 7378
+  WHEN `zone` = 'oot' THEN 7379
+  WHEN `zone` = 'butcher' THEN 7380
+  WHEN `zone` = 'oasis' THEN 7381
+  WHEN `zone` = 'nro' THEN 7382
+  WHEN `zone` = 'firiona' THEN 7383
+  WHEN `zone` = 'overthere' THEN 7384
+  WHEN `zone` = 'timorous' THEN 7385
+  WHEN `zone` = 'iceclad' THEN 7386
+END
+WHERE `launcher` = 'boats';
