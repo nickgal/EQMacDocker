503f65d76c8c5d0c05095a815c49bbe65dbb2e4b
Nick Gal <nickgal@me.com>
2023-09-01 02:17:48 +0000

    Add quarm submodules

---
+++ containers/database/quake_data.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS `quake_data`  (
+  `start_timestamp` int(11) UNSIGNED NOT NULL DEFAULT 0,
+  `next_timestamp` int(11) UNSIGNED NOT NULL DEFAULT 0,
+  `ruleset` int(11) NOT NULL DEFAULT 0
+);
